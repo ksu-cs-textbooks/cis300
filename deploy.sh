376efc12ddcc60aa0c60a2dcfc97531fdb8e5e6d
russfeld <russfeld@ksu.edu>
2020-06-05 15:57:55 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+cd ../cis300-book-html
+git pull
+git rm -rf .
+git clean -fxd
+
+cd ../cis300-book
 git pull --recurse-submodules
+
 hugo
+cd ../cis300-book-html
+git add .
+git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
+git push -u origin master
+
+ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/public_html/ && git pull"
+#ssh russfeld@cslinux.cs.ksu.edu "cd /home/r/russfeld/cc-website-html && git pull && rsync -aP --exclude='.git' /home/r/russfeld/cc-website-html/ /web/core"
+
 exit 0
