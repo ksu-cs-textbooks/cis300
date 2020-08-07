ff5c958e8dac69211a9896145bcb3f39ea7f6df4
Josh Weese <weeser@ksu.edu>
2020-08-07 13:56:09 -0500

    added messages to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+echo "pull and clean book html"
 cd ../cis300-book-html
 git pull
 git rm -rf .
 git clean -fxd
 
+echo "pull cis300-book and submodules"
 cd ../cis300-book
 git pull --recurse-submodules
 
+echo "running hugo to generate html"
 hugo
+echo "pushing HTML to cis300-book-html"
 cd ../cis300-book-html
 git add .
 git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
 git push -u origin master
 
-ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull"
+echo "pull on cs linux and copy to web"
+#ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull"
 ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull && rsync -aP --exclude='.git' /home/w/weeser/git-proj/cis300-book-html/ /web/cis300"
 
 exit 0
