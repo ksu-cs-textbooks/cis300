50e69a749df918bf403769f29153c0183677433d
Josh Weese <weeser@ksu.edu>
2020-08-11 15:14:03 -0500

    deploy testing

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,17 @@ TZ=America/Chicago
 LOG_FILE=/home/weeser/log/cis300deploy.log
 WEBHOOK_URL=https://outlook.office.com/webhook/e27fa57a-e17c-4e4c-aa3a-3eb5d88fc33f@d9a2fa71-d67d-4cb6-b541-06ccaa8013fb/IncomingWebhook/9a0f963775f24cd0b399dcde4aff31b4/a5f5dabb-1e16-468f-a332-9454044ffca0
 
-#exec > >(tee ${LOG_FILE}) 2>&1
+exec > >(tee -a ${LOG_FILE}) 2>
 current_time=$(date)
 send_message(){
     MESSAGE=$( echo ${3} | sed 's/"/\"/g' | sed "s/'/\'/g" )
-    JSON="{\"title\": \"${1}\", \"themeColor\": \"${2}\", \"text\": \"${MESSAGE}\" }"
+    JSON="{\"title\": \"${1}\", \"text\": \"${MESSAGE}\" }"
 
     # Post to Microsoft Teams.
     curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}"
 }
 
-echo ${current_TIME} >> ${LOG_FILE}
+echo ${current_time} >> ${LOG_FILE}
 echo "pull and clean book html git" >> ${LOG_FILE}
 send_message current_time "Deploy bot: pull and clean book html git"
 cd ../cis300-book-html
