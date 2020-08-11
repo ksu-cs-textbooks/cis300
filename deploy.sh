bc8a85b363005145616acc0e448489f8a75fd8e1
Josh Weese <weeser@ksu.edu>
2020-08-11 15:34:55 -0500

    deploy testing

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ send_message(){
     JSON="{\"title\": \"${1}\", \"text\": \"${2}\" }"
 
     # Post to Microsoft Teams.
-    curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}" 2> ${LOG_FILE}
+    curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}" 2>> ${LOG_FILE}
 }
 
 echo ${current_time} >> ${LOG_FILE}
@@ -22,7 +22,7 @@ git clean -fxd
 echo "pulling latest cis300-book master branch and submodules" >> ${LOG_FILE}
 send_message "Deploy bot" "pulling latest cis300-book master branch and submodules"
 cd ../cis300-book
-git pull --recurse-submodules 2> ${LOG_FILE}
+git pull --recurse-submodules 2>> ${LOG_FILE}
 
 echo "running hugo to generate html" >> ${LOG_FILE}
 send_message "Deploy bot" "running hugo to generate html"
@@ -31,14 +31,14 @@ echo "pushing HTML to cis300-book-html" >> ${LOG_FILE}
 send_message "Deploy bot" "pushing HTML to cis300-book-html"
 cd ../cis300-book-html
 git add . 2> ${LOG_FILE}
-git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`" 2> ${LOG_FILE}
-git push -u origin master 2> ${LOG_FILE}
+git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`" 2>> ${LOG_FILE}
+git push -u origin master 2>> ${LOG_FILE}
 
 echo "pull on cs linux and copy to web directory (this may take a while)" >> ${LOG_FILE}
 send_message "Deploy bot" "pull on cs linux and copy to web directory (this may take a while)"
 
 #ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull"
-ssh weeser@linux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull && rsync -aP --exclude='.git' /home/w/weeser/git-proj/cis300-book-html/ /web/cis300" 2> ${LOG_FILE}
+ssh weeser@linux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull && rsync -aP --exclude='.git' /home/w/weeser/git-proj/cis300-book-html/ /web/cis300" 2>> ${LOG_FILE}
 echo "completed" >> ${LOG_FILE}
 
 send_message "Deploy bot" "Finished deploying CIS 300 Book"
