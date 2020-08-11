7db2d0cedb35af700813f448af90e4da082afd10
Josh Weese <weeser@ksu.edu>
2020-08-11 15:27:44 -0500

    deploy testing

--- deploy.sh
+++ deploy.sh
@@ -13,34 +13,34 @@ send_message(){
 
 echo ${current_time} >> ${LOG_FILE}
 echo "pull and clean book html git" >> ${LOG_FILE}
-send_message current_time "Deploy bot: pull and clean book html git"
+send_message "Deploy bot" "pull and clean book html git"
 cd ../cis300-book-html
 git pull 2> ${LOG_FILE}
 git rm -rf .
 git clean -fxd
 
 echo "pulling latest cis300-book master branch and submodules" >> ${LOG_FILE}
-send_message current_time "Deploy bot: pulling latest cis300-book master branch and submodules"
+send_message "Deploy bot" "pulling latest cis300-book master branch and submodules"
 cd ../cis300-book
 git pull --recurse-submodules 2> ${LOG_FILE}
 
 echo "running hugo to generate html" >> ${LOG_FILE}
-send_message current_time "Deploy bot: running hugo to generate html"
+send_message "Deploy bot" "running hugo to generate html"
 hugo 2> ${LOG_FILE}
 echo "pushing HTML to cis300-book-html" >> ${LOG_FILE}
-send_message current_time "Deploy bot: pushing HTML to cis300-book-html"
+send_message "Deploy bot" "pushing HTML to cis300-book-html"
 cd ../cis300-book-html
 git add . 2> ${LOG_FILE}
 git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`" 2> ${LOG_FILE}
 git push -u origin master 2> ${LOG_FILE}
 
 echo "pull on cs linux and copy to web directory (this may take a while)" >> ${LOG_FILE}
-send_message current_time "Deploy bot: pull on cs linux and copy to web directory (this may take a while)"
+send_message "Deploy bot" "pull on cs linux and copy to web directory (this may take a while)"
 
 #ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull"
 ssh weeser@linux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull && rsync -aP --exclude='.git' /home/w/weeser/git-proj/cis300-book-html/ /web/cis300" 2> ${LOG_FILE}
 echo "completed" >> ${LOG_FILE}
 
-send_message current_time "Deploy bot: Finished deploying CIS 300 Book"
+send_message "Deploy bot" "Finished deploying CIS 300 Book"
 echo "______________________________________________________" >> ${LOG_FILE}
 exit 0
