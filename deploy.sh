009e4bddf5a3f108db3c94050e8d47a24a21fd6f
Josh Weese <weeser@ksu.edu>
2020-08-11 09:18:05 -0500

    touch up deploy

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 LOG_FILE=/home/weeser/log/cis300deploy.log
-exec > >(tee ${LOG_FILE}) 2>&1
+#exec > >(tee ${LOG_FILE}) 2>&1
 
-echo "pull and clean book html"
+date | tee ${LOG_FILE}
+echo "pull and clean book html" | tee ${LOG_FILE}
 cd ../cis300-book-html
 git pull
 git rm -rf .
 git clean -fxd
 
-echo "pull cis300-book and submodules"
+echo "pull cis300-book and submodules" | tee ${LOG_FILE}
 cd ../cis300-book
 git pull --recurse-submodules
 
-echo "running hugo to generate html"
+echo "running hugo to generate html" | tee ${LOG_FILE}
 hugo
-echo "pushing HTML to cis300-book-html"
+echo "pushing HTML to cis300-book-html" | tee ${LOG_FILE}
 cd ../cis300-book-html
 git add .
 git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
 git push -u origin master
 
-echo "pull on cs linux and copy to web"
+echo "pull on cs linux and copy to web" | tee ${LOG_FILE}
 #ssh weeser@cslinux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull"
 ssh weeser@linux.cs.ksu.edu "cd /home/w/weeser/git-proj/cis300-book-html/ && git pull && rsync -aP --exclude='.git' /home/w/weeser/git-proj/cis300-book-html/ /web/cis300"
-
+echo "completed" | tee ${LOG_FILE}
+date | tee ${LOG_FILE}
 exit 0
